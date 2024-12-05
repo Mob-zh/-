881549f7d33c4339c18274d0eed51767b6c71b19
Ruixuan Lin <1356088661@qq.com>
2024-12-05 23:30:47 +0800

    已完成登录与修改密码接口

--- models/student.go
+++ models/student.go
@@ -1,5 +1,7 @@
 package models
 
+import "attendance_uniapp/initializer"
+
 /*
 	学生表模型
 */
@@ -9,4 +11,12 @@ type Student struct {
 	StudentName string  `json:"student_name" gorm:"type:varchar(50);not null;comment:学生姓名;"`
 	StudentPwd  string  `json:"student_pwd" gorm:"type:varchar(60);not null;comment:学生密码;"`
 	Classes     []Class `gorm:"many2many:student_classes;" json:"classes"` // 自动创建中间表 student_classes
+}
+
+func GetStudentById(studentId string) (*Student, error) {
+	student := &Student{}
+	if err := initializer.DB.Where("student_id=?", studentId).First(&student).Error; err != nil {
+		return nil, err
+	}
+	return student, nil
 }
